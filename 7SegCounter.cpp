5d733b96574156d51c228bf8b860041f92e826a1
Kacper Lalik <kacper.lalik@interia.pl>
2024-04-30 00:34:04 +0200

    1stcommit

---
+++ 7SegCounter.cpp
@@ -0,0 +1,47 @@
+#include <avr/io.h>
+
+void display(unsigned char* pointarr[]){
+	unsigned char wyswietlacz[4]={0b0111,0b1011,0b1101,0b1110};
+	volatile unsigned int i,wait1,wait2;
+	for(wait1=0;wait1<1000;++wait1){
+		for(wait2=0;wait2<100;++wait2){
+			for(i=0;i<4;++i){
+					PORTB=wyswietlacz[i];
+					PORTD=~(*pointarr[i]);
+					PORTD=0b11111111;
+				}
+			}
+	}
+}
+
+void incerement(unsigned char* pointarr[],unsigned char arr[]){
+	++pointarr[0];
+	if(*pointarr[0]==0){
+		pointarr[0]=arr;
+		++pointarr[1];
+		if(*pointarr[1]==0){
+			pointarr[1]=arr;
+			++pointarr[2];
+			if(*pointarr[2]==0){
+				pointarr[2]=arr;
+				++pointarr[3];
+				if(*pointarr[3]==0) pointarr[3]=arr;
+			}
+		}
+	}
+}
+
+
+int main(void)
+{
+	unsigned char arr[11]={0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B,0};
+		unsigned char* pointarr[4]={arr,arr,arr,arr};
+		DDRB=0xFF;
+		DDRD=0xFF;
+    while (1) 
+    {
+		display(pointarr);
+		incerement(pointarr,arr);
+	}
+}
+
